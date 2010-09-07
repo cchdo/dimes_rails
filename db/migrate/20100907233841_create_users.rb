5586d59f43d282b8a5dd13bed7926f7e0c069242
Matt Shen <myeeshen@gmail.com>
2010-09-07 16:49:07 -0700

    Install Authlogic

---
+++ db/migrate/20100907233841_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :login,             :null => false
+      t.string :crypted_password,  :null => false
+      t.string :password_salt,     :null => false
+      t.string :persistence_token, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
