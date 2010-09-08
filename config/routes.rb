83db1df2d08cf4a49d23fe4b6218a8eb7017d3b1
Matt Shen <myeeshen@gmail.com>
2010-09-07 17:03:36 -0700

    Add views for user and allow for registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => :home
 
   map.resource :user_session
-  map.root :controller => :user_sessions, :action => :new
 
   map.resource :account, :controller => :users
   map.resources :users
