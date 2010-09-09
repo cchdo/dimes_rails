dc0f84e9b4384862d3a17d700d5228f617371705
Theresa Calderon <cgtheresa@gmail.com>
2010-09-09 16:23:48 -0700

    format results page, add DIMES investigator meeting page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -11,4 +11,72 @@ class ResultsController < ApplicationController
     render :partial=> "report_20100126"
   end
 
+  def report_20100204
+    render :partial=> "report_20100204"
+  end
+
+  def report_20100218
+    render :partial=> "report_20100218"
+  end
+
+  def report_20100303
+    render :partial=> "report_20100303"
+  end
+  
+  def report_20091206
+    render :partial=> "report_20091206"
+  end
+  
+  def report_20091211
+    render :partial=> "report_20091211"
+  end
+  
+  def report_20091220
+    render :partial=> "report_20091220"
+  end
+  
+  def us_introduction
+    render :partial=> "us_introduction"
+  end
+  
+  def us_participants
+    render :partial=> "us_participants"
+  end
+  
+  def report_20090115
+    render :partial=> "report_20090115"
+  end
+  
+  def report_20090117
+    render :partial=> "report_20090117"
+  end
+  
+  def report_20090120
+    render :partial=> "report_20090120"
+  end
+  
+  def report_20090122
+    render :partial=> "report_20090122"
+  end
+  
+  def report_20090129
+    render :partial=> "report_20090129"
+  end
+  
+  def report_20090201
+    render :partial=> "report_20090201"
+  end
+
+  def report_20090204
+    render :partial=> "report_20090204"
+  end
+
+  def report_20090206
+    render :partial=> "report_20090206"
+  end
+
+  def report_20090219
+    render :partial=> "report_20090219"
+  end
+
 end
