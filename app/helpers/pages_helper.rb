b6f10b1081c8dd3951fd78d6c9c192a5a931fcc4
Matt Shen <myeeshen@gmail.com>
2014-01-15 10:38:23 -0800

    Add and reformat publications

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,4 +3,8 @@ module PagesHelper
         content_tag(:p, image_tag(imgsrc,
                                   {:alt => cap, :title => cap}.merge(opts))) + content_tag(:p, cap, :class => :cap)
     end
+
+    def pdf_icon
+        image_tag('pdf.jpg', :height => '13')
+    end
 end
